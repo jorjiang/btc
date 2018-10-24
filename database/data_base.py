2d7ef1e551e503e770b8b84f0c17f7a6b840555a
Jiang Ji <jiang.ji@check24.de>
2018-10-24 22:36:19 +0200

    class BtcDatabase stage1 finished

---
+++ database/data_base.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from database.config import BlockchainInfoConfig
+import datetime
+class BtcDatabase():
+    def __init__(self, update_interval=3):
+        self.current_time = datetime.datetime.now()
+        self.update_interval = update_interval
+
+        self.charts = ['my-wallet-n-users']
+        # self.charts = BlockchainInfoConfig["ts_charts"]
+        self.time_span = BlockchainInfoConfig["time_span"]
+        self.chart_format = BlockchainInfoConfig["format"]
+
+        self.url_header = BlockchainInfoConfig["url_header"]
+
+    def create_btc_database(self):
+        data = pd.DataFrame()
+        for chart in self.charts:
+            chart_url = self.url_header + chart + "?timespan={0}&format={1}".format(self.time_span, self.chart_format)
+            print(chart_url)
+            ts = pd.read_csv(chart_url)
+            data = pd.concat([data, ts])
+        self.data = data
+
+    def update_databasa(self):
+
+
+def test_btc_database():
+    btc_database = BtcDatabase()
+    btc_database.create_btc_database()
+    print(btc_database.data)
+
+test_btc_database()
