92e089ed54f493feefefb3db5e4e9d0516bf7d59
Jiang Ji <jiang.ji@check24.de>
2018-10-28 02:33:04 +0100

    adapt save data to pickle, and if data is more than 3 days old, redownload

--- btc_data.py
+++ btc_data.py
@@ -1,16 +1,18 @@
-from datetime import datetime
+from datetime import datetime, date, timedelta
 from time import sleep
 import pandas as pd
 from tqdm import tqdm
-from database.config import BlockchainInfoConfig
-import MySQLdb
+import os
+from config import BlockchainInfoConfig, BtcDataConfig
+from sklearn.externals import joblib
 
 
-class BtcDatabase():
+class BlockchaininfoData():
     def __init__(self, update_interval=3):
         self.current_time = datetime.now()
         self.update_interval = update_interval
 
+        self.ts_data_file = BtcDataConfig['blockchaininfo_ts_data_file']
         self.charts = BlockchainInfoConfig["ts_charts"]
         self.time_span = BlockchainInfoConfig["time_span"]
         self.chart_format = BlockchainInfoConfig["format"]
@@ -19,6 +21,7 @@ class BtcDatabase():
         self.starting_date = '1/3/2009'
         self.ending_date = self.current_time.strftime("%Y-%m-%d %H:%M:%S")
         self.time_series = pd.Series(pd.date_range(start=self.starting_date, end=self.ending_date).date)
+        self.data = self._get_data()
 
     def create_btc_dataframe(self, charts=None):
         if charts is None:
@@ -31,15 +34,11 @@ class BtcDatabase():
             chart_name = chart.replace('-', '_')
             chart_url = self.url_header + chart + "?timespan={0}&format={1}".format(self.time_span, self.chart_format)
             ts = pd.read_csv(chart_url, header=None, names=['time', '{}'.format(chart_name)])
-            ts.time = ts.time.apply(self._str_to_date_time)
+            ts.time = ts.time.apply(self._datetime_str_to_date)
             ts = self._adapt_time_seire(ts)
             data[chart_name] = ts[chart_name]
         self.data = data
         return data
-
-    def create_btc_database(self):
-        self.data = self.create_btc_database()
-
 
     def _adapt_time_seire(self, ts: pd.DataFrame) -> pd.Series:
         # aggregate daily data to its mean
@@ -52,15 +51,24 @@ class BtcDatabase():
         ts = ts.interpolate().fillna(0)
         return ts
 
-    def _str_to_date_time(self, datetime_str: str) -> datetime:
+    def _get_data(self):
+        if not os.path.isfile(self.ts_data_file) \
+                or self.current_time.date() - self._get_file_last_modified_date(
+                self.ts_data_file) > timedelta(self.update_interval):
+            data = self._create_date_file()
+
+        else:
+            data = joblib.load(self.ts_data_file)
+
+        return data
+
+    def _create_date_file(self):
+        data = self.create_btc_dataframe()
+        joblib.dump(data, self.ts_data_file)
+        return data
+
+    def _get_file_last_modified_date(self, file: str) -> datetime.date:
+        return date.fromtimestamp(os.stat(file).st_mtime)
+
+    def _datetime_str_to_date(self, datetime_str: str) -> datetime.date:
         return datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S').date()
-
-    def update_databasa(self):
-
-        pass
-
-
-def test_btc_database():
-    btc_database = BtcDatabase()
-    btc_database.create_btc_database()
-    print(btc_database.data)
