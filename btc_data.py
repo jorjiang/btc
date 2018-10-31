e038682cabd54e9d5334e9afac41a2680dcd17bf
Jiang Ji <jiang.ji@check24.de>
2018-10-31 15:45:03 +0100

    fix bug

--- btc_data.py
+++ btc_data.py
@@ -1,21 +1,19 @@
+import os
 from datetime import datetime, date, timedelta
 from time import sleep
+
 import pandas as pd
-from tqdm import tqdm
-import os
 from config import BlockchainInfoConfig, BtcDataConfig
 from sklearn.externals import joblib
+from tqdm import tqdm
+
 
 class WebData():
-    def __int__(self):
+    def __init__(self):
         self.current_time = datetime.now()
         self.starting_date = '1/3/2009'
         self.ending_date = self.current_time.strftime("%Y-%m-%d %H:%M:%S")
         self.time_series = pd.Series(pd.date_range(start=self.starting_date, end=self.ending_date).date)
-        self.data = self._get_data()
-
-    def _get_data(self):
-        raise NotImplementedError
 
     def _adapt_time_seire(self, ts: pd.DataFrame) -> pd.Series:
         # aggregate daily data to its mean
@@ -31,16 +29,20 @@ class WebData():
     def _datetime_str_to_date(self, datetime_str: str) -> datetime.date:
         return datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S').date()
 
+    def _create_btc_dataframe(self):
+        raise NotImplementedError
+
+
 class BlockchaininfoData(WebData):
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
         self.data_identifier = 'BI'
         self.charts = BlockchainInfoConfig["ts_charts"]
         self.time_span = BlockchainInfoConfig["time_span"]
         self.chart_format = BlockchainInfoConfig["format"]
 
         self.url_header = BlockchainInfoConfig["url_header"]
-        self.data = self._create_btc_dataframe()
+        self.data = self._create_btc_dataframe(self.charts)
 
     def _create_btc_dataframe(self, charts=None):
         if charts is None:
@@ -49,7 +51,7 @@ class BlockchaininfoData(WebData):
             charts = [c.replace('_', '-') for c in charts]
         data = pd.DataFrame(index=self.time_series)
         for chart in tqdm(charts):
-            sleep(0.5)
+            sleep(1)
             chart_name = chart.replace('-', '_') + '_' + self.data_identifier
             chart_url = self.url_header + chart + "?timespan={0}&format={1}".format(self.time_span, self.chart_format)
             ts = pd.read_csv(chart_url, header=None, names=['time', '{}'.format(chart_name)])
@@ -59,25 +61,24 @@ class BlockchaininfoData(WebData):
         return data
 
 
-
 class BitinfochartData(WebData):
     def __init__(self):
         super().__init__()
 
+
 class BtcData():
     def __init__(self, update_interval=3):
+        self.current_time = datetime.now()
         self.ts_data_file = BtcDataConfig['blockchaininfo_ts_data_file']
-        self.blockchaininfo_data = BlockchaininfoData()
-        self.btc_data = self.blockchaininfo_data.data
-        self.current_time = self.blockchaininfo_data.current_time
+
         self.update_interval = update_interval
+        self.data = self._get_data()
 
     def _get_data(self):
         if not os.path.isfile(self.ts_data_file) \
                 or self.current_time.date() - self._get_file_last_modified_date(
-                self.ts_data_file) > timedelta(self.update_interval):
+            self.ts_data_file) > timedelta(self.update_interval):
             data = self._create_date_file()
-
         else:
             data = joblib.load(self.ts_data_file)
 
@@ -87,8 +88,7 @@ class BtcData():
         return date.fromtimestamp(os.stat(file).st_mtime)
 
     def _create_date_file(self):
-        joblib.dump(self.btc_data, self.ts_data_file)
-        return self.btc_data
-
-
-
+        self.blockchaininfo_data = BlockchaininfoData()
+        data = self.blockchaininfo_data.data
+        joblib.dump(data, self.ts_data_file)
+        return data
